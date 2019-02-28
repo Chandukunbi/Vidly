f5ca2ea8010bc7efb0d9a10472419b11b301ecb4
Chandrakant <chandukunbi@gmail.com>
2019-02-28 14:47:31 +0530

    Display Genre In Movies List

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,44 +11,29 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            var movies = new List<Movie>
-            {
-                new Movie {Name="Shrek" },
-                new Movie {Name="Wall-e" },
-            };
-            var viewModel = new MoviesViewModel()
-            {
-                Movies = movies
-            };
-            return View(viewModel);
+            var movies = _context.Movies.Include( m => m.Genre).ToList();            
+            return View(movies);
         }
 
-        public ActionResult Details()
+        public ActionResult Details(int id)
         {
-            return View();
+            var movie = _context.Movies.Include( m => m.Genre).SingleOrDefault(m => m.Id == id);
+            return View(movie);
         }
-
-        // GET: Movies
-        public ActionResult Random()
-        {
-            var movie = new Movie(){Name = "Shrek!"};
-            var customers = new List<Customer>
-            {
-                new Customer {Name="Customer 1" },
-                new Customer {Name="Customer 2" }
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie=movie,
-                Customers=customers
-            };
-
-            return View(viewModel);           
-        }
-
-      
+              
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,17 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        public DateTime ReleasedDate { get; set; }
+        [Required]
+        public DateTime AddedDate { get; set; }
+        [Required]
+        public int Stock { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
     }
 
     // /movies/random
