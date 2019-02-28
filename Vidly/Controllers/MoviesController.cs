78feff9e5dbd35719cc51d89e0d3a9475b929c9a
Chandrakant <chandukunbi@gmail.com>
2019-02-28 18:35:44 +0530

    Completed Section 4 Excercise

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -29,10 +29,49 @@ namespace Vidly.Controllers
             return View(movies);
         }
 
-        public ActionResult Details(int id)
+        public ActionResult New()
         {
-            var movie = _context.Movies.Include( m => m.Genre).SingleOrDefault(m => m.Id == id);
-            return View(movie);
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel()
+            {
+                Genres = genres
+            };
+            return View("MovieForm",viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+            
+            var viewModel = new MovieFormViewModel()
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList(),
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if(movie.Id == 0)
+                _context.Movies.Add(movie);
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleasedDate = movie.ReleasedDate;
+                movieInDb.AddedDate = movie.AddedDate;
+                movieInDb.GenreId = movie.GenreId;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
               
     }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,16 +9,25 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
         [Required]
+        [Display(Name="Release Date")]
         public DateTime ReleasedDate { get; set; }
-        [Required]
-        public DateTime AddedDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Number in Stock")]
         [Required]
         public int Stock { get; set; }
-        [Required]
+
         public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]        
         public byte GenreId { get; set; }
     }
 
