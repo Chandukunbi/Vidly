4276b96bfc87941d80daef71bc552b7c0d2c52e3
Chandrakant <chandukunbi@gmail.com>
2019-03-02 19:23:42 +0530

    Added jquery Datatable and consumed web api using jquery

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using System;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -27,7 +28,10 @@ namespace Vidly.Controllers.Api
         // GET /api/customers
         public IHttpActionResult GetCustomers()
         {
-            IEnumerable <CustomerDto> customers = _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+            IEnumerable <CustomerDto> customers = _context.Customers
+                .Include(c => c.MembershipType)
+                .ToList()
+                .Select(Mapper.Map<Customer, CustomerDto>);
 
             return Ok(customers);
         }
